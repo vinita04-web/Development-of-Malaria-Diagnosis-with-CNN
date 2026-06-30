e3311efb35611212798e3007236ad074e027286d
Vinita Vinayak Pawar <pawarvinitav@gmail.com>
2026-06-30 15:07:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load trained model
+model = tf.keras.models.load_model("malaria_model.keras")
+
+st.title("🩸 Malaria Detection from Blood Cell Images 🧬")
+st.subheader("Deep Learning Based CNN Classifier")
+uploaded_file = st.file_uploader(
+    "Choose an image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file).convert("RGB")
+
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    # Resize image
+    image = image.resize((50, 50))
+
+    img_array = np.array(image)
+
+    img_array = img_array / 255.0
+
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+
+    probability = float(prediction[0][0])
+
+    if probability > 0.5:
+        st.error("Prediction: Parasitized (Malaria Infected)")
+        st.write(f"Confidence: {probability:.2%}")
+    else:
+        st.success("Prediction: Uninfected")
+        st.write(f"Confidence: {(1 - probability):.2%}")
